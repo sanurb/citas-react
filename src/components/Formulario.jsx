f95f886d87de14f93369b875436dae2756292663
Santiago Urbano <david.urbano@uao.edu.co>
2023-04-07 15:30:12 -0500

    feat(Formulario.jsx): add form to add patients with name, owner, admission date and symptoms
    feat(ListadoPacientes.jsx): add Paciente component and props to ListadoPacientes component. Add conditional rendering for when there are no patients.
    
    feat(Paciente.jsx): add Paciente component with props for paciente, setPaciente, and eliminarPaciente functions

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,12 +1,79 @@
+import {useState} from "react";
+
 export const Formulario = () => {
   return (
     <div className="md:w-1/2 lg:w-2/5">
-        <h2 className="font-black text-stone-700 text-3xl text-center">Formulario</h2>
+      <h2 className="font-black text-stone-700 text-3xl text-center">
+        Formulario
+      </h2>
 
-        <p className="text-lg mt-5 text-center mb-10">
+      <p className="text-lg mt-5 text-center mb-10">
         Añade Pacientes y {""}
         <span className="text-indigo-600 font-bold ">Administralos</span>
       </p>
+
+      <form className="bg-white shadow-md rounded-lg py-10 px-5">
+        <div className="mb-5">
+          <label
+            className="block text-gray-700 uppercase font-bold"
+            htmlFor="mascota"
+          >
+            Nombre Mascota
+          </label>
+          <input
+            id="mascota"
+            type="text"
+            placeholder="Nombre de la Mascota"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+          />
+        </div>
+        <div className="mb-5">
+          <label
+            htmlFor="propietario"
+            className="block text-gray-700 uppercase font-bold"
+          >
+            Nombre Propietario
+          </label>
+          <input
+            id="propietario"
+            type="text"
+            placeholder="Nombre del Propietario"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+          />
+        </div>
+        <div className="mb-5">
+          <label
+            htmlFor="alta"
+            className="block text-gray-700 uppercase font-bold"
+          >
+            Alta
+          </label>
+          <input
+            id="alta"
+            type="date"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+          />
+        </div>
+
+        <div className="mb-5">
+          <label
+            htmlFor="sintomas"
+            className="block text-gray-700 uppercase font-bold"
+          >
+            Síntomas
+          </label>
+          <textarea
+            id="sintomas"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            placeholder="Describe los Síntomas"
+          />
+        </div>
+
+        <input
+          type="submit"
+          className="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-colors"
+        />
+      </form>
     </div>
-  )
-}
+  );
+};
--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,14 +1,42 @@
-export default function ListadoPacientes() {
-  return (
-    <div className="w-1/2 lg:w-3/5">
-      <h2 className="font-black text-stone-700 text-3xl text-center">
-        Listado Pacientes
-      </h2>
+import Paciente from "./Paciente"
 
-      <p className="text-xl mt-5 mb-10 text-center">
-        Administra tus {""}
-        <span className="text-indigo-600 font-bold ">Pacientes y Citas</span>
-      </p>
+export default function ListadoPacientes({
+  pacientes,
+  setPaciente,
+  eliminarPaciente,
+}) {
+  return (
+    <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
+      {pacientes && pacientes.length ? (
+        <>
+          <h2 className="font-black text-3xl text-center text-stone-700 ">Listado Pacientes</h2>
+          <p className="text-xl mt-5 mb-10 text-center">
+            Administra tus {""}
+            <span className="text-indigo-600 font-bold ">
+              Pacientes y Citas
+            </span>
+          </p>
+
+          {pacientes.map((paciente) => (
+            <Paciente
+              key={paciente.id}
+              paciente={paciente}
+              setPaciente={setPaciente}
+              eliminarPaciente={eliminarPaciente}
+            />
+          ))}
+        </>
+      ) : (
+        <>
+          <h2 className="font-black text-3xl text-center text-stone-700 ">No hay pacientes</h2>
+          <p className="text-xl mt-5 mb-10 text-center">
+            Comienza agregando pacientes {""}
+            <span className="text-indigo-600 font-bold ">
+              y aparecerán en este lugar
+            </span>
+          </p>
+        </>
+      )}
     </div>
   );
 }
