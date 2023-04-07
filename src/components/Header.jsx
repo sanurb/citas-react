76c730e1522c04abe117ccf40c57d500dc364ca0
Santiago Urbano <david.urbano@uao.edu.co>
2023-04-07 12:08:44 -0500

    feat: add .gitignore, index.html, package.json, and postcss.config.js files
    feat(App.jsx): add Header, Formulario and ListadoPacientes components to App component
    feat(public/vite.svg): add Vite logo to public folder
    
    feat(Formulario.jsx): add Formulario component
    feat(Header.jsx): add Header component
    feat(ListadoPacientes.jsx): add ListadoPacientes component
    feat(index.css): add tailwindcss styles
    feat(main.jsx): add ReactDOM.createRoot to render App component
    feat(tailwind.config.js): add tailwindcss configuration file
    
    feat(vite.config.js): add react plugin to Vite configuration

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+
+    return (
+        <h1 className="font-black text-stone-700 text-5xl text-center md:w-2/3 mx-auto">
+            Seguimiento Pacientes {""} 
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-indigo-700">Veterinaria</span>
+        </h1>
+    )
+}
---
+++ src/components/ListadoPacientes.jsx
@@ -0,0 +1,14 @@
+export default function ListadoPacientes() {
+  return (
+    <div className="w-1/2 lg:w-3/5">
+      <h2 className="font-black text-stone-700 text-3xl text-center">
+        Listado Pacientes
+      </h2>
+
+      <p className="text-xl mt-5 mb-10 text-center">
+        Administra tus {""}
+        <span className="text-indigo-600 font-bold ">Pacientes y Citas</span>
+      </p>
+    </div>
+  );
+}
